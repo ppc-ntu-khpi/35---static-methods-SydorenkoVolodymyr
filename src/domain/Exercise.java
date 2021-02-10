6969db140a9db575d5137ee11f622c380235fbf2
SydorenkoVolodymyr <74309456+SydorenkoVolodymyr@users.noreply.github.com>
2021-02-10 21:04:07 +0200

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,16 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    
+    public static byte[] Encrypt(String text, String keyWord)
+    {
+        byte[] arr = text.getBytes();
+        byte[] key_arr = keyWord.getBytes();
+        byte[] resultat = new byte[arr.length];
+        for(int i=1; i<arr.length; i++)
+        {
+            resultat[i] = (byte) (arr[i] ^ key_arr[i % key_arr.length]);
+        }
+        return resultat;
     }
 }
